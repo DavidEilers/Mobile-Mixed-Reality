bc4d806b873167a0b9c2b43afea78e8c237edb95
David Eilers <david.eilers@s2017.tu-chemnitz.de>
2021-01-13 19:24:24 +0100

    Initial ArCore commit(dummy implementation)

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,124 @@
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 #include <jni.h>
 #include <string>
+#include "arcore_c_api.h"
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring JNICALL
+
+extern "C" {
+
+
+JNIEXPORT jstring JNICALL
 Java_com_example_teampraktikum_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        JNIEnv *env,
+        jobject activity/* this */, jobject context) {
+    std::string errorStr = "ERROR";
+    std::string successStr = "Success";
+    //nativeOnCreate();
+    //__android_log_print(ANDROID_LOG_VERBOSE, "TeamPraktikum", "The value is %d", test());
+    return env->NewStringUTF("Hello World!");
 }
+
+bool install_requested_ = false;
+ArSession *ar_session_ = nullptr;
+ArFrame *ar_frame_ = nullptr;
+int width_ = 1;
+int height_ = 1;
+int display_rotation_ = 0;
+
+JNIEXPORT void JNICALL
+Java_com_example_teampraktikum_MainActivity_nativeOnResume(
+        JNIEnv *env,
+        jobject activity,
+        jobject context) {
+    if (ar_session_ == nullptr) {
+        bool user_requested_install = !install_requested_;
+
+        ArInstallStatus install_status;
+        // Ensure Google Play Services for AR and ARCore device profile data are
+        // installed and up to date.
+        ArStatus error = ArCoreApk_requestInstall(
+                env, activity, user_requested_install, &install_status);
+        if (error != AR_SUCCESS) {
+            // Inform user of error.
+            return;
+        }
+
+        switch (install_status) {
+            case AR_INSTALL_STATUS_INSTALLED:
+                break;
+            case AR_INSTALL_STATUS_INSTALL_REQUESTED:
+                // When this method returns `AR_INSTALL_STATUS_INSTALL_REQUESTED`:
+                // 1. This activity will be paused.
+                // 2. The user is prompted to install or update Google Play
+                //    Services for AR (market://details?id=com.google.ar.core).
+                // 3. ARCore downloads the latest device profile data.
+                // 4. This activity is resumed. The next invocation of
+                //    ArCoreApk_requestInstall will either return
+                //    `AR_INSTALL_STATUS_INSTALLED` or throw an exception if the
+                //    installation or update did not succeed.
+                install_requested_ = true;
+                return;
+        }
+
+        // Request camera permissions.
+
+        error = ArSession_create(env, context, &ar_session_);
+        if (error != AR_SUCCESS) {
+            // Inform user of error.
+            __android_log_print(ANDROID_LOG_VERBOSE, "TeamPraktikum",
+                                "ArSession couldn't be created ArStatusCode: %d", error);
+            return;
+        }
+
+
+        // Configure session
+        ArConfig *ar_config = nullptr;
+        ArConfig_create(ar_session_, &ar_config);
+        ArConfig_setDepthMode(ar_session_, ar_config, AR_DEPTH_MODE_DISABLED);
+        ArConfig_setInstantPlacementMode(ar_session_, ar_config,
+                                         AR_INSTANT_PLACEMENT_MODE_LOCAL_Y_UP);
+        if (ArSession_configure(ar_session_, ar_config) != AR_SUCCESS) {
+            return;
+        }
+        ArConfig_destroy(ar_config);
+
+        ArFrame_create(ar_session_, &ar_frame_);
+        ArSession_setDisplayGeometry(ar_session_, display_rotation_, width_, height_);
+        __android_log_print(ANDROID_LOG_VERBOSE, "TeamPraktikum",
+                            "Created AR Session successfully");
+    }
+
+    // Normal onResume behavior
+    const ArStatus status = ArSession_resume(ar_session_);
+    if (status != AR_SUCCESS) {
+        return;
+    }
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_teampraktikum_MainActivity_onDisplayGeometryChanged(
+        JNIEnv *env,
+        jobject activity,
+        int display_rotation,
+        int width,
+        int height
+) {
+    display_rotation_ = display_rotation;
+    width_ = width;
+    height_ = height;
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_teampraktikum_MainActivity_onDrawFrame(
+        JNIEnv *env,
+        jobject activity) {
+    //glClearColor(0.7f,0.0f,0.0f,1.0f);
+    // glClear(GL_COLOR_BUFFER_BIT);
+}
+}
+
+
+
+
--- app/src/main/java/com/example/teampraktikum/MainActivity.java
+++ app/src/main/java/com/example/teampraktikum/MainActivity.java
@@ -2,29 +2,153 @@ package com.example.teampraktikum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.hardware.display.DisplayManager;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+//import com.google.android.material.snackbar.Snackbar;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity implements GLSurfaceView.Renderer, DisplayManager.DisplayListener {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+    private GLSurfaceView surfaceView;
+    private int viewPortWidth;
+    private int viewPortHeight;
+    private boolean viewPortChanged;
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+    private static final int CAMERA_PERMISSION_CODE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        surfaceView = (GLSurfaceView) findViewById(R.id.surfaceview);
+        // Set up renderer.
+        surfaceView.setPreserveEGLContextOnPause(true);
+        surfaceView.setEGLContextClientVersion(2);
+        surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
+        surfaceView.setRenderer(this);
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        surfaceView.setWillNotDraw(false);
 
+        //JniInterface.assetManager = getAssets();
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI(getApplicationContext()));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, CAMERA_PERMISSION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this, new String[]{CAMERA_PERMISSION}, CAMERA_PERMISSION_CODE);
+            return;
+        }
+        nativeOnResume(getApplicationContext());
+        surfaceView.onResume();
+//        loadingMessageSnackbar =
+//                Snackbar.make(
+//                        HelloArActivity.this.findViewById(android.R.id.content),
+//                        "Searching for surfaces...",
+//                        Snackbar.LENGTH_INDEFINITE);
+//        // Set the snackbar background to light transparent black color.
+//        loadingMessageSnackbar.getView().setBackgroundColor(0xbf323232);
+//        loadingMessageSnackbar.show();
+//        planeStatusCheckingHandler.postDelayed(
+//                planeStatusCheckingRunnable, SNACKBAR_UPDATE_INTERVAL_MILLIS);
+
+        // Listen to display changed events to detect 180° rotation, which does not cause a config
+        // change or view resize.
+        getSystemService(DisplayManager.class).registerDisplayListener(this, null);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        surfaceView.onPause();
+        getSystemService(DisplayManager.class).unregisterDisplayListener(this);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
+     *
+     * @param context
      */
-    public native String stringFromJNI();
+    public native String stringFromJNI(Context context);
+
+    public native void nativeOnResume(Context context);
+
+    public native void onDisplayGeometryChanged(int display_rotation, int width, int height);
+
+    public native void onDrawFrame();
+
+    @Override
+    public void onDisplayAdded(int displayId) {
+
+    }
+
+    @Override
+    public void onDisplayRemoved(int displayId) {
+
+    }
+
+    @Override
+    public void onDisplayChanged(int displayId) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.7f, 0.0f, 0.0f, 1.0f);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        viewPortWidth = width;
+        viewPortHeight = height;
+        viewPortChanged = true;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        synchronized (this) {
+            if (viewPortChanged == true) {
+                int displayRotation = getWindowManager().getDefaultDisplay().getRotation();
+                onDisplayGeometryChanged(displayRotation, viewPortWidth, viewPortHeight);
+                viewPortChanged = false;
+            }
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        }
+    }
 }
