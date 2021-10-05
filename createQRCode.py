fc1af4ffb720cd87ff2569658b7dad2234d6653d
David Eilers <david.eilers@s2017.tu-chemnitz.de>
2021-10-05 15:13:54 +0200

    Small previously uncommited changes

--- createQRCode.py
+++ createQRCode.py
@@ -9,6 +9,8 @@ def showImage():
 	hostname = socket.gethostname()
 	local_ip = socket.gethostbyname(hostname)
 	img = pyqrcode.create(local_ip)
+	print("Your IP:"+local_ip)
 	print(img.terminal(quiet_zone=1))
+	return local_ip
 
 
